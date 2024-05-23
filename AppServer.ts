ddba4781638db3adaf2ab17ceafa94007f238060
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2024-05-23 16:43:18 -0700

    init repo

--- AppServer.ts
+++ AppServer.ts
@@ -1,17 +1,6 @@
 // import * as dotenv from 'dotenv';
 // import {App} from './App';
 
-// dotenv.config();
-
-// const port = process.env.PORT;
-// const dbUser = process.env.DB_USER;
-// const dbPassword = process.env.DB_PASSWORD;
-// const mongoDBConnection = 'mongodb://localhost:27017/my_database';
-// console.log("server db connection URL " + mongoDBConnection);
-
-// let server: any = new App(mongoDBConnection).expressApp;
-// server.listen(port);
-// console.log("server running in port " + port);
 
 import * as dotenv from 'dotenv';
 import {App} from './App';
